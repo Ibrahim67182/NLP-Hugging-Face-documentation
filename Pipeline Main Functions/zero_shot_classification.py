ce9e79ead84e1b9898be9e2630e23784f97dd932
Ibrahim Junaid <115927887+Ibrahim67182@users.noreply.github.com>
2024-11-14 01:21:11 +0500

    Update zero_shot_classification.py

--- Pipeline Main Functions/zero_shot_classification.py
+++ Pipeline Main Functions/zero_shot_classification.py
@@ -7,9 +7,9 @@ from transformers import pipeline
 
 classifier = pipeline("zero-shot-classification")
 classifier(
-    "This is a course about the Transformers library",   //context 
+    "This is a course about the Transformers library",   # o  a given context  and labels it will give predicted score for each label associated with context
 
-    candidate_labels=["education", "politics", "business"],   labels to get their score
+    candidate_labels=["education", "politics", "business"],  # labels to get their score
 )
 
 
