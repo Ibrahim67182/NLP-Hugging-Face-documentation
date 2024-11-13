c5b0dc8a2ee3e7489b7cac3c38acedb13a251def
Ibrahim Junaid <115927887+Ibrahim67182@users.noreply.github.com>
2024-11-14 01:14:28 +0500

    Update ques_and_ans.py

--- Pipeline Main Functions/ques_and_ans.py
+++ Pipeline Main Functions/ques_and_ans.py
@@ -1,1 +1,15 @@
 
+from transformers import pipeline
+
+
+
+# question answering model 
+
+
+question_answerer = pipeline("question-answering")
+answered= question_answerer(
+    question="Where do I work?",
+    context="My name is Sylvain and I work at Hugging Face in Brooklyn",
+)
+
+print(answered)
