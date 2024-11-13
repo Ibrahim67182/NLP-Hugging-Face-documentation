a5d337b739fff6548d2f9ccceef953d8bbc198a2
Ibrahim Junaid <115927887+Ibrahim67182@users.noreply.github.com>
2024-11-14 01:15:00 +0500

    Update sentiment_analysis.py

--- Pipeline Main Functions/sentiment_analysis.py
+++ Pipeline Main Functions/sentiment_analysis.py
@@ -1,1 +1,12 @@
+from transformers import pipeline
+
+
+# sentiment analysis 
+
+analysis = pipeline("sentiment-analysis")        # gives whther statement is positive and score 
+
+
+analyze= analysis("I have tired of my daily hectic and boring routine")     #it should be negative 
+print(analyze) 
+
 
