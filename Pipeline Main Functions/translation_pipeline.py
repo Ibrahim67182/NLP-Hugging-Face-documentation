7eaeb40d105289e6777657e74613da8bbc511fe2
Ibrahim Junaid <115927887+Ibrahim67182@users.noreply.github.com>
2024-11-14 01:19:28 +0500

    Update translation_pipeline.py

--- Pipeline Main Functions/translation_pipeline.py
+++ Pipeline Main Functions/translation_pipeline.py
@@ -1,1 +1,10 @@
 
+
+from transformers import pipeline
+
+# translation text 
+translation = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ru")      # using nlp model of hugging face translator eng to russian
+
+translated= translation("My name is ibrahim")
+
+print(translated)
