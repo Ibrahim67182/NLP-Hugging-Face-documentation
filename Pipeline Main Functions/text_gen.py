80086667758449fe2464409fdca321c5a5f3c826
Ibrahim Junaid <115927887+Ibrahim67182@users.noreply.github.com>
2024-11-14 01:17:59 +0500

    Update text_gen.py

--- Pipeline Main Functions/text_gen.py
+++ Pipeline Main Functions/text_gen.py
@@ -1,1 +1,16 @@
 
+
+
+from transformers import pipeline
+
+# text generation 
+
+
+generator = pipeline("text-generation", model="distilgpt2")
+genrated= generator(
+    "once upon a time",
+    max_length=15,
+    num_return_sequences=2,truncation= True                 #it will generate  two sequences or sentences of len 15 of a given context
+)
+
+print(genrated)
