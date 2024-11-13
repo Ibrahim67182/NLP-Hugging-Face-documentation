3d94b428d351cfc4914fc43a9f17df463b6955f7
Ibrahim Junaid <115927887+Ibrahim67182@users.noreply.github.com>
2024-11-14 01:13:04 +0500

    Update named_entity_recognition.py

--- Pipeline Main Functions/named_entity_recognition.py
+++ Pipeline Main Functions/named_entity_recognition.py
@@ -1,1 +1,12 @@
+from transformers import pipeline
+
+# named entity recognition (ner)  find the entities in text such as person organization 
+# person is PER  , organization is ORG and    location is LOC
+
+
+ner = pipeline("ner", grouped_entities= True)
+
+recognized =ner("My name is ibrahim and i study at FAST NUCES and i live in karachi")
+ 
+print(recognized)
 
