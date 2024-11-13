74ad3d75476360f91c2b0dedacc4c9857b717733
Ibrahim Junaid <115927887+Ibrahim67182@users.noreply.github.com>
2024-11-14 01:11:56 +0500

    Update mask_fill.py

--- Pipeline Main Functions/mask_fill.py
+++ Pipeline Main Functions/mask_fill.py
@@ -1,1 +1,10 @@
+from transformers import pipeline
+
+
+# mask filling model 
+
+unmasker = pipeline("fill-mask")
+unmasked= unmasker("The AI course will teach you all about <mask> skills.", top_k=2)   # it fill two words in mask 
+
+print(unmasked)
 
