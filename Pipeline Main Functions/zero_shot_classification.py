74cad37e477f1ac039800bb6df421f35e5f7542b
Ibrahim Junaid <115927887+Ibrahim67182@users.noreply.github.com>
2024-11-14 01:20:04 +0500

    Update zero_shot_classification.py

--- Pipeline Main Functions/zero_shot_classification.py
+++ Pipeline Main Functions/zero_shot_classification.py
@@ -1,1 +1,15 @@
+from transformers import pipeline
+
+#zero shot classification
+
+
+
+
+classifier = pipeline("zero-shot-classification")
+classifier(
+    "This is a course about the Transformers library",   //context 
+
+    candidate_labels=["education", "politics", "business"],   labels to get their score
+)
+
 
