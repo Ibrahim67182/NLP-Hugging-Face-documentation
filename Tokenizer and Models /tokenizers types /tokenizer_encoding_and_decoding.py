21be43f7102e03e26faf0912cb2bea5cc1622295
Ibrahim Junaid <115927887+Ibrahim67182@users.noreply.github.com>
2024-11-22 01:28:40 +0500

    Update tokenizer_encoding_and_decoding.py

--- Tokenizer and Models /tokenizers types /tokenizer_encoding_and_decoding.py
+++ Tokenizer and Models /tokenizers types /tokenizer_encoding_and_decoding.py
@@ -1,1 +1,25 @@
+# encoding process in tokenizers from raw text to input ids 
+
+
+from transformers import AutoTokenizer
+
+name = "distilbert-base-uncased-finetuned-sst-2-english"
+
+tokenizer = AutoTokenizer.from_pretrained(name)
+
+text = "I’ve been waiting for a HuggingFace course my whole life."
+
+tokens = tokenizer.tokenize(text)
+
+ids = tokenizer.convert_tokens_to_ids(tokens)
+
+print(ids)
+
+
+# decoding the string from input ids  reverse process
+
+result  = tokenizer.decode([2026, 2171, 2003, 13477, 12022, 14326, 1998, 1045, 2572, 19875, 3076, 999])
+
+print(result)
+
 
